2b23065c14c6231c8f06f4a927f3ae8b717d3ba7
Rodrj1 <rodrigo_mdq93@hotmail.com>
2023-03-10 21:17:09 -0300

    Created localStorage for the app

--- src/stores/useProjectStore.ts
+++ src/stores/useProjectStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { ProjectProps } from '../types';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface ProjectStore {
   projects: Array<ProjectProps>;
@@ -11,21 +12,32 @@ interface ProjectStore {
   updateStoredProjects: (projects: ProjectProps[]) => void;
 }
 
-export const useProjectStore = create<ProjectStore>((set) => ({
-  projects: [],
-  selectedProject: '',
-  createProject: (project: ProjectProps) =>
-    set((state) => ({
-      projects: [...state.projects, project],
-    })),
-  deleteProject: (projectName: string) =>
-    set((state) => ({
-      projects: [
-        ...state.projects.filter((projects) => projects.name != projectName),
-      ],
-    })),
-  selectProject: (projectName: string) => set({ selectedProject: projectName }),
-  deselectProject: () => set({ selectedProject: '' }),
-  updateStoredProjects: (projects: ProjectProps[]) =>
-    set({ projects: projects }),
-}));
+export const useProjectStore = create<ProjectStore>()(
+  persist(
+    (set) => ({
+      projects: [],
+      selectedProject: '',
+      createProject: (project: ProjectProps) =>
+        set((state) => ({
+          projects: [...state.projects, project],
+        })),
+      deleteProject: (projectName: string) =>
+        set((state) => ({
+          projects: [
+            ...state.projects.filter(
+              (projects) => projects.name != projectName
+            ),
+          ],
+        })),
+      selectProject: (projectName: string) =>
+        set({ selectedProject: projectName }),
+      deselectProject: () => set({ selectedProject: '' }),
+      updateStoredProjects: (projects: ProjectProps[]) =>
+        set({ projects: projects }),
+    }),
+    {
+      name: 'todo-storage',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
